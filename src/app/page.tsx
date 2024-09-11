6c0f607b2477d513289f60cea8fe748d5fdc8679
sameer tahir <sameertahir024@gmail.com>
2024-09-11 15:18:04 +0500

    first comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
-import Projects from "@/components/Projects";
 import SidePro from "@/components/SidePro";
 import Skills from "@/components/Skills";
-import Footer from "@/components/ui/Footer";
 import MarqueeDemo from "@/components/ui/MarqueeDemo";
 
 export default function Home() {
   return (
     <div>
-      
       <Hero />
       <About />
       <SidePro />
